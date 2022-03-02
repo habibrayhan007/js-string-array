e85b3acd5edd0a0eeb83749921580ab42e66d8b8
habib <rayhanhabib56@gmail.com>
2022-03-02 11:51:47 +0600

    learn toLowerCase

---
+++ case-convert.js
@@ -0,0 +1,11 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobasi';
+const search = 'valo';
+
+
+const userInput = 'blACkPinK';
+const savedUserName = 'blackPink';
+//console.log(userInput.toLowerCase());
+console.log(savedUserName.toLowerCase());
+if(userInput.toLocaleLowerCase() == savedUserName){
+    console.log('user exists');
+}
